3a18de39cd8732df721afe868f94fd44a8e05940
Naveen Yerravarapu <naveen.yerravarapu@modak.com>
2022-09-20 17:34:45 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "gce" {
   source = "./module"
   machine_type = var.machine_type
-  vm_name = var.name
+  vm_name = var.vm_name
   zone = var.zone
+  image = var.image
+  network_name = var.network_name
 }
