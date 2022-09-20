3c3ac85c52ceaf509ce3988bd57baf34aba48d13
Naveen Yerravarapu <naveen.yerravarapu@modak.com>
2022-09-20 16:44:09 +0530

    changed backend

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-      google = {
-        source = "hashicorp/google"
-        version = "4.27.0"
-      }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.27.0"
     }
+  }
   backend "gcs" {
-        bucket = "state-file-mt4015"
-        prefix = "#{backend-value}#-statefile"
-        credentials = "serviceaccount.json"
+    bucket = var.bucket_name
+    prefix = "#{backend-value}#-statefile"
+    credentials = "serviceaccount.json"
   }
 }
